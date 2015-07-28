68dcbf43a62a1ee12335c8f583ee7616fa794adf
Ben Kitzelman <benkitzelman@gmail.com>
2015-07-28 14:11:31 +1000

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports.bind = function bind(fn) {
+  var callback = fn;
+
+  Object.keys(process.namespaces).forEach(function (name) {
+    callback = process.namespaces[name].bind( callback );
+  });
+
+  return callback;
+}
