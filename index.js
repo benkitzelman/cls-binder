beffdaec30a93f6595e4d30129f463ebe0d4434a
Ben Kitzelman <benkitzelman@gmail.com>
2015-07-28 15:43:48 +1000

    robustifying

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports.bind = function bind(fn) {
   var callback = fn;
 
-  Object.keys(process.namespaces).forEach(function (name) {
-    callback = process.namespaces[name].bind( callback );
+  Object.keys(process.namespaces || {}).forEach(function (name) {
+    callback = process.namespaces[ name ].bind( callback );
   });
 
   return callback;
